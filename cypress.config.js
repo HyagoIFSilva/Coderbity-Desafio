525aae316bf6aeeef8bd7d3957b8899ebfcfd5ca
hyagoIFSilva <hyagodw32@outlook.com>
2026-05-08 00:49:46 -0300

    Feat: Adição de arquivo de suporte e2e com manipulador de exceções não capturadas e limpeza dos comentários de configuração do Cypress.

--- cypress.config.js
+++ cypress.config.js
@@ -4,11 +4,11 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://teste-colmeia-qa.colmeia-corp.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+   
     },
-    // Desabilitar o cache para rodar sempre em ambiente limpo se necessário
+ 
     testIsolation: true,
-    // Tamanho da janela padrão do Cypress
+    
     viewportWidth: 1280,
     viewportHeight: 720,
   },
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,12 +1,7 @@
-// ***********************************************************
-// Arquivo de suporte principal para a automação.
-// Ele é carregado automaticamente antes dos arquivos de teste.
-// ***********************************************************
+
 
 import './commands'
 
-// Desativando exceções não capturadas para evitar que os testes 
-// falhem por erros do próprio frontend do site que não estamos validando agora
 Cypress.on('uncaught:exception', (err, runnable) => {
   return false
 })
